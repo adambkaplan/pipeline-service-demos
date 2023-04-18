598105c60f4437a028a82ea73e58dee836c2bef8
Adam Kaplan <adam.kaplan@redhat.com>
2023-04-18 14:41:10 -0400

    Demo - Sprint 235
    Repeat of the 232 demo, except it works end to end. This doesn't go into
    the technical details of getting S3 to work. It does decode the log data
    and print it to screen.
    
    Signed-off-by: Adam Kaplan <adam.kaplan@redhat.com>

---
+++ demos/sprint-235/README.md
@@ -0,0 +1,12 @@
+# Sprint 235 Demo
+
+## Highlights
+
+- Results e2e: REST + S3 + Logs
+
+## Demo Notes
+
+- Run the Pipeline Service test script beforehand. This gives us enough
+  pipelineruns to work with.
+- Bonus points - delete the containers the pipeline ran on (drain node,
+  shutdown/restart crc cluster)
---
+++ demos/sprint-235/demo.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+
+set -e
+
+# shellcheck source=./hack/demo-magic.sh
+source ./hack/demo-magic.sh
+
+clear
+
+p "Pipeline Service Demo - Sprint 235"
+
+clear
+
+NAMESPACE="plnsvc-tests"
+
+p "Tekton Results - REST + S3 + Logs!"
+p "Results can be queried at a route endpoint, under a RESTful path"
+p "Here are the pipelines that have been run:"
+tkn pipelinerun list -o name -n $NAMESPACE
+pipeline_name=$(tkn pipelineruns list -n $NAMESPACE -o name | head -1)
+p "Let's query the result record for $pipeline_name!"
+
+clear
+
+RESULT_UID=$(kubectl get "$pipeline_name" -n "$NAMESPACE" -o yaml | yq .metadata.uid)
+p "The UID for this PipelineRun is $RESULT_UID"
+p "This UID is used to find the result record directly."
+
+RESULT_ROUTE=$(kubectl get route tekton-results -n tekton-results --template='{{.spec.host}}')
+ROOT_URL="https://$RESULT_ROUTE/apis/results.tekton.dev/v1alpha2/parents"
+QUERY_URL="$ROOT_URL/$NAMESPACE/results/$RESULT_UID"
+
+p "Let's look at the result summary:"
+echo "curl -s --insecure $QUERY_URL | jq"
+
+RESULTS_SA="tekton-results-tests"
+token=$(kubectl create token "$RESULTS_SA" -n "$NAMESPACE")
+
+curl -s --insecure \
+  -H "Authorization: Bearer $token" \
+  -H "Accept: application/json" \
+  "$QUERY_URL" | jq
+
+wait
+clear
+
+p "...and the records:"
+echo "curl -s --insecure $QUERY_URL/records | jq '.records[] | {name: .name, type: .data.type}'"
+
+curl -s --insecure \
+  -H "Authorization: Bearer $token" \
+  -H "Accept: application/json" \
+  "$QUERY_URL/records" | \
+  jq '.records[] | {name: .name, type: .data.type}'
+
+wait
+clear
+
+p "...and the logs!"
+echo "curl -s --insecure $QUERY_URL/logs | jq '.records[] | {name: .name, type: .data.type}'"
+
+curl -s --insecure \
+  -H "Authorization: Bearer $token" \
+  -H "Accept: application/json" \
+  "$QUERY_URL/logs" | \
+  jq '.records[] | {name: .name, type: .data.type}'
+
+wait
+clear
+
+p "We can try and get the first log..."
+firstLog=$(curl -s --insecure -H "Authorization: Bearer $token" -H "Accept: application/json" "$QUERY_URL/logs" | jq -r '.records[0] | .name')
+echo "curl -s --insecure $ROOT_URL/$firstLog | jq"
+
+curl -s --insecure \
+  -H "Authorization: Bearer $token" \
+  -H "Accept: application/json" \
+  "$ROOT_URL/$firstLog" | \
+  jq
+
+wait
+p "Notice that we have data - which is base64 encoded."
+
+clear
+p "Decoding we get..."
+
+curl -s --insecure \
+  -H "Authorization: Bearer $token" \
+  -H "Accept: application/json" \
+  "$ROOT_URL/$firstLog" | \
+  jq -r '.result.data' | \
+  base64 -d
+
+wait
+clear
+
+p "This concludes the demo. Enjoy!"
+
+clear
